90491f58a61f34b0c13c14a2a4ab1b0c0514ee1d
Carlos <crgarcia@live.com.ar>
2018-08-22 14:46:57 +0200

    Added both projects

---
+++ src/FrontEnd/FrontEndSite/Controllers/FireworksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEndSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FireworksController : ControllerBase
+    {
+        [HttpGet]
+        public int NumberOfFireworks()
+        {
+            return 3;
+        }
+    }
+}
